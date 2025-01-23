f60bac1181fc2a2d86f32ff3c59f04242da50875
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2025-01-23 18:52:31 +0000

    create podman network

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM debian:bookworm
 
+ENV RSYSLOG_DEBUGLOG="true"
+
 RUN apt-get update -y
 RUN apt-get install -y rsyslog
+
+ENTRYPOINT ["/usr/sbin/rsyslogd", "-d", "-n"]
--- start_pods.sh
+++ start_pods.sh
@@ -7,21 +7,25 @@ output_pod=output
 echo "building pod ${pod}..."
 podman build --quiet --file Dockerfile --tag ${pod} . > /dev/null
 
+echo "starting network..."
+podman network create ${pod} > /dev/null || true
+
 echo "starting ${input_pod}..."
 podman run \
 	--detach \
 	--replace \
 	-v "$(pwd)/input/rsyslog.conf:/etc/rsyslog.conf":Z \
-	-p "10514:514" \
+	-p "10514:10514" \
 	--name ${input_pod} \
-	${pod} \
-	/usr/sbin/rsyslogd -n
+	--network ${pod} \
+	${pod}
 
 echo "starting ${output_pod}..."
 podman run \
 	--detach \
 	--replace \
 	-v "$(pwd)/output/rsyslog.conf:/etc/rsyslog.conf":Z \
+	-v "$(pwd)/outputs:/outputs" \
 	--name ${output_pod} \
-	${pod} \
-	/usr/sbin/rsyslogd -n
+	--network ${pod} \
+	${pod}
