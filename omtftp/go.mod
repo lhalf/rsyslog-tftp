ddf2f5177cd3d2c5ed3c02cfbf6b41c2a945ac21
Laurence Halfpenny <laurence.halfpenny@halfpenny.net>
2025-02-13 20:49:52 +0000

    add initial test for omtftp module

---
+++ omtftp/go.mod
@@ -0,0 +1,5 @@
+module github.com/lhalf/rsyslog-tftp/omtftp
+
+go 1.23.4
+
+require github.com/oakdoor/go-tftp v0.0.0-20250128151209-bceeaa902f1f
---
+++ omtftp/omtftp_test.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"strings"
+	"testing"
+)
+
+type MockTFTPClient struct {
+	PutCalled    bool
+	PutURL       string
+	PutReader    io.Reader
+	PutSize      int64
+	PutError     error
+	ReceivedData string
+	CallCount    int
+}
+
+func (m *MockTFTPClient) Put(url string, reader io.Reader, size int64) (err error) {
+	m.PutCalled = true
+	m.PutURL = url
+	m.PutReader = reader
+	m.PutSize = size
+	m.CallCount++
+
+	if reader != nil {
+		data, err := io.ReadAll(reader)
+		if err != nil {
+			return err
+		}
+		m.ReceivedData = string(data)
+	}
+	return m.PutError
+}
+
+func TestProcessTransactions_SingleMessage(t *testing.T) {
+	input := `BEGIN TRANSACTION
+Message 1
+COMMIT TRANSACTION
+`
+	reader := bufio.NewReader(strings.NewReader(input))
+	mockClient := &MockTFTPClient{}
+	address := "test_address"
+
+	processTransactions(reader, mockClient, address)
+
+	if !mockClient.PutCalled {
+		t.Errorf("Expected Put to be called")
+	}
+
+	if mockClient.PutURL != address {
+		t.Errorf("Expected PutURL to be '%s', got '%s'", address, mockClient.PutURL)
+	}
+
+	expectedData := "Message 1\n"
+
+	if mockClient.ReceivedData != expectedData {
+		t.Errorf("Expected PutReader data to be '%s', but got '%s'", expectedData, mockClient.ReceivedData)
+	}
+
+	if mockClient.PutSize != 0 {
+		t.Errorf("Expected PutSize to be 0, but got %d", mockClient.PutSize)
+	}
+}
